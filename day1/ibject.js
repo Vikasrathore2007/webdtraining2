9b8ced9095403fb34ae953e371c217d646e14a0e
Vikasrathore2007 <vikasrathore21752@gmail.com>
2026-08-20 10:55:12 +0530

    add

---
+++ day1/ibject.js
@@ -0,0 +1,20 @@
+//let c= [10,20,30,40,506,60];
+ //let data = c.pop();
+ //console.log(data);
+ //let date = c.push(80);
+// console.log(date);
+ //data = c.shift();
+ //console.log(data);
+
+ //data = c.unshift();
+//console.log(data);
+//console.log(c)
+
+let name="vikas rathore";
+console.log(name.charAt(3));
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(1,6));
+let s=name.Include("rathore");
+s.replace("Rathore",sir);
+console.log(name.Include("rathore"));
