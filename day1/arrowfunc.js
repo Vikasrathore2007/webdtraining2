1b19867bf1501821d2307d2f148e4449b3c953e5
Vikasrathore2007 <vikasrathore21752@gmail.com>
2026-09-03 11:43:48 +0530

    arrow function

--- day1/arrowfunc.js
+++ day1/arrowfunc.js
@@ -9,4 +9,18 @@ hello();
 greet();
 Hello();
 physics=(marks)=>`you scored:${marks}`;
-console.log(physics(95));
+console.log(physics(95);
+
+
+ p=(n)=>{
+    for(let i=1;i<=n;i++){
+        let r=' ';
+        for(let j=1;j<=i;j++){
+            r +=' * ';
+        }
+        console.log(r);
+    }
+
+};
+console.log("-----pattern-----");
+p(5);
