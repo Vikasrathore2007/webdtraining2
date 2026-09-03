b99241125651635114a3892792029b1677f9045e
Vikasrathore2007 <vikasrathore21752@gmail.com>
2026-09-03 11:53:31 +0530

    rest operator

--- day1/restop.js
+++ day1/restop.js
@@ -1,1 +1,9 @@
 //rest operator in java script
+function sum(...value){
+     let total=0;
+     for(let n of value){
+        total =total+n;
+     }
+     return total;
+    }
+   console.log(`sum is:${sum(10,20,30,405,50)}`);// connecting string template
